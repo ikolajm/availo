7cf863d026eb2228ee27bec7514c7df5f77d719e
ikolajm <ikolajm@gmail.com>
2026-01-27 13:39:28 -0500

    intiial commit of api code

---
+++ services/api/src/index.js
@@ -0,0 +1,22 @@
+// import express from "express";
+// import { ApolloServer } from "apollo-server-express";
+// import { typeDefs } from "./schema";
+// import { resolvers } from "./resolvers";
+
+// async function start() {
+//   const app = express();
+
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//   });
+
+//   await server.start();
+// //   server.applyMiddleware({ app });
+
+//   app.listen(4000, () => {
+//     console.log("GraphQL API running at http://localhost:4000/graphql");
+//   });
+// }
+
+// // start();
